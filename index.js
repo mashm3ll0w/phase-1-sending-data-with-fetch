63afc41addf3843c7fd801ab5dde3162de0470cb
Charles Swaleh <charlswall@live.com>
2022-12-15 16:40:32 +0300

    Completed sending data with fetch lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    
+  return fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name: name,
+        email: email
+      })
+    })
+    .then( (response) => {
+      return response.json()
+    })
+    .then( (userObj) => {
+      document.body.innerHTML = userObj.id
+    })
+    .catch( (error) => {
+      document.body.innerHTML = error.message
+    })
+}
